a75e408b24f27dd0efc9d41c99ab7513cf45b6eb
Rizky <74634775+harrypoter78@users.noreply.github.com>
2026-05-14 19:47:41 +0700

    Delete run_server.bat

--- run_server.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "d:\Documents\Laravel\mental-health-id"
-echo Memulai Laravel development server...
-echo Server akan berjalan di http://localhost:8000
-echo Tekan Ctrl+C untuk menghentikan server
-php artisan serve --host=0.0.0.0 --port=8000
-pause
